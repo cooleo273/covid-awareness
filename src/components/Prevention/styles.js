bac990c8dcd6c801e2f45181871b1aa150549b50
Leo <leulteferi273@gmail.com>
2020-09-27 23:28:02 -0700

    formed prevention and modified it

---
+++ src/components/Prevention/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const container3 = styled.div`
+    display:grid;
+    grid-template-rows:1fr 1fr 1fr 1fr;
+`
+export const WearMask = styled.div`
+    display:grid;
+    grid-template-columns:1fr 1fr;
+` 
+export const WashHands = styled.div`
+    display:grid;
+    grid-template-columns:1fr 1fr;
+` 
+export const NoseRag = styled.div`
+    display:grid;
+    grid-template-columns:1fr 1fr;
+` 
+export const AvoidContact = styled.div`
+    display:grid;
+    grid-template-columns:1fr 1fr;
+` 
